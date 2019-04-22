70affd11136d6eda3c3f5f5531998023de4dfe5f
Han, Eunbyeol <hanstar961@hotmail.com>
2019-04-23 01:12:54 +1000

    character move function simplify

--- main.py
+++ main.py
@@ -102,32 +102,41 @@ def character_on_map():
     return map_line
 
 
-def right_side_checking():
-    if square[row][col + 2] == empty_space:
+def check_side(x, y):
+    if square[x][y] == empty_space:
         return '1'
     else:
         return '0'
 
+def right_side_checking():
+    return check_side(new_position[0], new_position[1] + 2)
 
 def left_side_checking():
-    if square[row][col - 2] == empty_space:
-        return '1'
-    else:
-        return '0'
-
+    return check_side(new_position[0], new_position[1] - 2)
 
 def down_side_checking():
-    if square[row + 2][col] == empty_space:
-        return '1'
-    else:
-         return '0'
-
+    return check_side(new_position[0] + 2, new_position[1])
 
 def up_side_checking():
-    if square[row - 2][col] == empty_space:
-        return '1'
-    else:
-        return '0'
+    return check_side(new_position[0] - 2, new_position[1])
+
+def make_new_position(direction, row, col):
+    square[row].pop(col)
+    square[row].insert(col, empty_space)
+
+    if direction == 'right':
+        col += 2
+    elif direction == 'left':
+        col -= 2
+    elif direction =='down':
+        row += 2
+    elif direction == 'up':
+        row -= 2
+
+    square[row].pop(col)
+    square[row].insert(col, character)
+
+    return [row, col]
 
 
 
@@ -144,57 +153,24 @@ change = "".join(square[row])
 new_position = [row, col]
 while True:
     key = screen.get_input()
-    screen.print_on_screen("                                       You entered " + key, 0)
-
+    screen.print_on_screen("                                       Position " + str(new_position), 0)
 
     if key == 'right':
         if new_position[1] < size * 2 - 1 and right_side_checking() == '1':
-            square[row].pop(col)
-            square[row].insert(col, empty_space)
-            col += 2
-            new_position = [row, col]
-            square[row].pop(col)
-            square[row].insert(col, character)
-            change = "".join(square[row])
-        else:
-            pass
+            new_position = make_new_position(key, new_position[0], new_position[1])
 
 
-
-
-
-    if key == 'left':
+    elif key == 'left':
         if new_position[1] > 1 and left_side_checking() == '1':
-            square[row].pop(col)
-            square[row].insert(col, empty_space)
-            col -= 2
-            new_position = [row, col]
-            square[row].pop(col)
-            square[row].insert(col, character)
-            change = "".join(square[row])
+            new_position = make_new_position(key, new_position[0], new_position[1])
 
-    if key == 'down':
+    elif key == 'down':
         if new_position[0] < size * 2 - 1 and down_side_checking() == '1':
-            square[row].pop(col)
-            square[row].insert(col, empty_space)
-            row += 2
-            new_position = [row, col]
-            square[row].pop(col)
-            square[row].insert(col, character)
-            change = "".join(square[row])
+            new_position = make_new_position(key, new_position[0], new_position[1])
 
-    if key == 'up':
+    elif key == 'up':
         if new_position[0] > 1 and up_side_checking() == '1':
-            square[row].pop(col)
-            square[row].insert(col, empty_space)
-            row -= 2
-            new_position = [row, col]
-            square[row].pop(col)
-            square[row].insert(col, character)
-            change = "".join(square[row])
+            new_position = make_new_position(key, new_position[0], new_position[1])
 
     for r_number, r in enumerate(square):
         screen.print_on_screen("".join(r), r_number)
-
-
-
